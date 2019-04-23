39909a7ebc2fc7f0d8a1ad67e110a8ec76e76e53
Nobuchika Tanaka <codefox.7799@gmail.com>
2019-04-24 07:10:16 +0900

    Update role

--- my-new-role/molecule/default/tests/test_default.py
+++ my-new-role/molecule/default/tests/test_default.py
@@ -6,9 +6,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
-
+def test_molecule_hello_world_file(host):
+    f = host.file('/tmp/molecule_hello_world')
     assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert f.content_string in 'Molecule Hello World!'
